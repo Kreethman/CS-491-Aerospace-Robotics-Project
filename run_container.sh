2f1fa85057839b8d7ff2989a25a1caebfad02205
kreethman <jre2002@gmail.com>
2026-04-18 19:21:06 -0700

    Removing pesky absolute path

--- run_container.sh
+++ run_container.sh
@@ -4,5 +4,5 @@ docker run -it \
 	-e DISPLAY=$DISPLAY \
 	--device /dev/dri:/dev/dri \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	-v "/shared/School Stuff/CS 491 Aerospace/project/src":/app/aerial_robotics_ws/src \
+	-v "./src":/app/aerial_robotics_ws/src \
 	aerobotics:latest
